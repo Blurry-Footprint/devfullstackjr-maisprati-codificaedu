138a78c623d2af96ac29e1a3bd1e9746ea5522e9
Richard F. Neo <200711513+Blurry-Footprint@users.noreply.github.com>
2025-06-26 23:58:07 +0000

    Delete devfullstackjr-maisprati-codificaedu/atividades-obrigatorias/lista-01 directory

--- devfullstackjr-maisprati-codificaedu/atividades-obrigatorias/lista-01/9-contagem-regressiva.js
+++
@@ -1,8 +0,0 @@
-// Contagem regressiva de 10 até 1
-console.log("Contagem regressiva iniciando:");
-
-for (let i = 10; i >= 1; i--) {
-  console.log(i); // Exibe o valor atual de i
-}
-
-console.log("Contagem finalizada!");
